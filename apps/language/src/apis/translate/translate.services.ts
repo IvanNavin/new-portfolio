37d60335700d76ef86c89cb8cb98458e8cc09502
IvanHolovko777 <ivan@worktechlabs.io>
2024-10-31 09:42:22 +0100

    move fetch translate to the services

--- apps/language/src/apis/translate/translate.services.ts
+++ apps/language/src/apis/translate/translate.services.ts
@@ -1,6 +1,20 @@
 export const translate = async (word: string) => {
-  const res = await fetch(`/api/translate?word=${word}`);
-  const getWord = await res.json();
+  const url = `https://lingvanex-translate.p.rapidapi.com/translate?from=en_GB&to=ru&text=${word}`;
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-rapidapi-key': process.env.X_RAPIDAPI_KEY!,
+      'x-rapidapi-host': process.env.X_RAPIDAPI_HOST!,
+      'Content-Type': 'application/json',
+    },
+  };
 
-  return getWord?.result;
+  try {
+    const response = await fetch(url, options);
+    const getWord = await response.json();
+
+    return getWord?.result;
+  } catch (error) {
+    console.error(error);
+  }
 };
